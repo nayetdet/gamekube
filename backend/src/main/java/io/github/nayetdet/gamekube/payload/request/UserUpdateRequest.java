c09c358e33e7b44c30096fd5b58524eff5f4938e
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-14 05:31:54 +0000

    feat: modify Keycloak realm configuration to adjust username validation rules

--- backend/src/main/java/io/github/nayetdet/gamekube/payload/request/UserUpdateRequest.java
+++ backend/src/main/java/io/github/nayetdet/gamekube/payload/request/UserUpdateRequest.java
@@ -2,7 +2,6 @@ package io.github.nayetdet.gamekube.payload.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +11,7 @@ import lombok.Setter;
 public class UserUpdateRequest {
 
   @NotBlank
-  @Size(min = 5, max = 50)
-  @Pattern(
-      regexp = "^(?!_+$)[a-z0-9_]+$",
-      message = "Username should only contain letters and numbers")
+  @Size(min = 3, max = 50)
   @Schema(example = "string")
   private String username;
 
