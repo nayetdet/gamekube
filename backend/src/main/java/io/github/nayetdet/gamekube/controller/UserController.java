44026a3bf0b6b7e769404ead761e08962ec81863
nayetdet <jpmoreiracampos21@gmail.com>
2026-08-14 01:22:24 +0000

    feat: implement user management endpoints in UserController and update UserControllerDocs with OpenAPI annotations

--- backend/src/main/java/io/github/nayetdet/gamekube/controller/UserController.java
+++ backend/src/main/java/io/github/nayetdet/gamekube/controller/UserController.java
@@ -1,11 +1,73 @@
 package io.github.nayetdet.gamekube.controller;
 
 import io.github.nayetdet.gamekube.controller.docs.UserControllerDocs;
+import io.github.nayetdet.gamekube.exception.UserNotFoundException;
+import io.github.nayetdet.gamekube.payload.query.UserQuery;
+import io.github.nayetdet.gamekube.payload.query.page.ApplicationPage;
+import io.github.nayetdet.gamekube.payload.request.UserUpdateRequest;
+import io.github.nayetdet.gamekube.payload.response.UserResponse;
+import io.github.nayetdet.gamekube.security.authorization.annotation.PreAuthorizeAdmin;
+import io.github.nayetdet.gamekube.security.authorization.annotation.PreAuthorizeUser;
+import io.github.nayetdet.gamekube.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
-public class UserController implements UserControllerDocs {}
+public class UserController implements UserControllerDocs {
+
+  private final UserService userService;
+
+  @Override
+  @PreAuthorizeAdmin
+  @GetMapping
+  public ResponseEntity<ApplicationPage<UserResponse>> search(@ParameterObject UserQuery query) {
+    return ResponseEntity.ok(userService.search(query));
+  }
+
+  @Override
+  @PreAuthorizeAdmin
+  @GetMapping("/{username}")
+  public ResponseEntity<UserResponse> find(@PathVariable String username) {
+    return userService
+        .find(username)
+        .map(ResponseEntity::ok)
+        .orElseThrow(UserNotFoundException::new);
+  }
+
+  @Override
+  @PreAuthorizeUser
+  @PostMapping("/{username}/reset-email")
+  public ResponseEntity<Void> changeEmail(@PathVariable String username) {
+    userService.updateEmail(username);
+    return ResponseEntity.noContent().build();
+  }
+
+  @Override
+  @PreAuthorizeUser
+  @PutMapping("/{username}")
+  public ResponseEntity<Void> update(
+      @PathVariable String username, @RequestBody @Valid UserUpdateRequest request) {
+    userService.update(username, request);
+    return ResponseEntity.noContent().build();
+  }
+
+  @Override
+  @PreAuthorizeUser
+  @DeleteMapping("/{username}")
+  public ResponseEntity<Void> delete(@PathVariable String username) {
+    userService.delete(username);
+    return ResponseEntity.noContent().build();
+  }
+}
--- backend/src/main/java/io/github/nayetdet/gamekube/controller/docs/UserControllerDocs.java
+++ backend/src/main/java/io/github/nayetdet/gamekube/controller/docs/UserControllerDocs.java
@@ -1,6 +1,105 @@
 package io.github.nayetdet.gamekube.controller.docs;
 
+import io.github.nayetdet.gamekube.payload.query.UserQuery;
+import io.github.nayetdet.gamekube.payload.query.page.ApplicationPage;
+import io.github.nayetdet.gamekube.payload.request.UserUpdateRequest;
+import io.github.nayetdet.gamekube.payload.response.UserResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@Tag(name = "Users", description = "Endpoints for managing games")
-public interface UserControllerDocs {}
+@Tag(name = "Users", description = "Endpoints for managing users")
+public interface UserControllerDocs {
+
+  @Operation(
+      summary = "Search all users",
+      security = @SecurityRequirement(name = "bearerAuth"),
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Ok",
+            content = @Content(schema = @Schema(implementation = ApplicationPage.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error",
+            content = @Content)
+      })
+  ResponseEntity<ApplicationPage<UserResponse>> search(@ParameterObject UserQuery query);
+
+  @Operation(
+      summary = "Find a user by username",
+      security = @SecurityRequirement(name = "bearerAuth"),
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "Ok",
+            content = @Content(schema = @Schema(implementation = UserResponse.class))),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error",
+            content = @Content)
+      })
+  ResponseEntity<UserResponse> find(@PathVariable String username);
+
+  @Operation(
+      summary = "Reset user email",
+      security = @SecurityRequirement(name = "bearerAuth"),
+      responses = {
+        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error",
+            content = @Content)
+      })
+  ResponseEntity<Void> changeEmail(@PathVariable String username);
+
+  @Operation(
+      summary = "Update a user",
+      security = @SecurityRequirement(name = "bearerAuth"),
+      requestBody =
+          @io.swagger.v3.oas.annotations.parameters.RequestBody(
+              required = true,
+              content = @Content(schema = @Schema(implementation = UserUpdateRequest.class))),
+      responses = {
+        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+        @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error",
+            content = @Content)
+      })
+  ResponseEntity<Void> update(@PathVariable String username, @Valid UserUpdateRequest request);
+
+  @Operation(
+      summary = "Delete a user by username",
+      security = @SecurityRequirement(name = "bearerAuth"),
+      responses = {
+        @ApiResponse(responseCode = "204", description = "No Content", content = @Content),
+        @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content),
+        @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content),
+        @ApiResponse(responseCode = "404", description = "Not Found", content = @Content),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Internal Server Error",
+            content = @Content)
+      })
+  ResponseEntity<Void> delete(@PathVariable String username);
+}
