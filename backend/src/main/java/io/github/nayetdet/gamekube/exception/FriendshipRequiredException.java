f10e51ac6dfdb45daabbc2f8463904216d38c832
Rodrigo Sousa <rodrigosousasales9@gmail.com>
2026-09-02 21:12:33 -0300

    feat: add DTOs, mapper, and exception for 1-to-1 chat

---
+++ backend/src/main/java/io/github/nayetdet/gamekube/exception/FriendshipRequiredException.java
@@ -0,0 +1,18 @@
+package io.github.nayetdet.gamekube.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class FriendshipRequiredException extends BaseException {
+
+  private static final String DEFAULT_ERROR_MESSAGE = "Users must be friends to chat";
+
+  public FriendshipRequiredException() {
+    super(DEFAULT_ERROR_MESSAGE);
+  }
+
+  public FriendshipRequiredException(String message) {
+    super(message);
+  }
+}
---
+++ backend/src/main/java/io/github/nayetdet/gamekube/payload/request/ChatMessageRequest.java
@@ -0,0 +1,25 @@
+package io.github.nayetdet.gamekube.payload.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageRequest {
+
+  @NotBlank
+  @Size(min = 3, max = 50)
+  private String recipientUsername;
+
+  @NotBlank
+  @Size(max = 2000)
+  private String content;
+}
