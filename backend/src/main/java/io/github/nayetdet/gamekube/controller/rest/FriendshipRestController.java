f3de25af6b90c45eac4d9b1471ccbf3a8c98465b
nayetdet <jpmoreiracampos21@gmail.com>
2026-09-20 03:33:56 +0000

    feat(friendship): change accept and reject endpoints to use PATCH method

--- backend/src/main/java/io/github/nayetdet/gamekube/controller/rest/FriendshipRestController.java
+++ backend/src/main/java/io/github/nayetdet/gamekube/controller/rest/FriendshipRestController.java
@@ -15,9 +15,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,7 +62,7 @@ public class FriendshipRestController implements FriendshipRestControllerDocs {
 
   @Override
   @PreAuthorizeUser
-  @PutMapping("/{username}/accept")
+  @PatchMapping("/{username}/accept")
   public ResponseEntity<FriendshipResponse> accept(
       @PathVariable String username, Principal principal) {
     return ResponseEntity.ok(friendshipService.decide(principal.getName(), username, true));
@@ -70,7 +70,7 @@ public class FriendshipRestController implements FriendshipRestControllerDocs {
 
   @Override
   @PreAuthorizeUser
-  @PutMapping("/{username}/reject")
+  @PatchMapping("/{username}/reject")
   public ResponseEntity<FriendshipResponse> reject(
       @PathVariable String username, Principal principal) {
     return ResponseEntity.ok(friendshipService.decide(principal.getName(), username, false));
